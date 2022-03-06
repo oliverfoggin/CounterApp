659a79ddb3510a814ee6ee13e1bd97b376779169
Oliver Foggin <oliverfoggin@me.com>
2022-03-06 23:45:10 +0000

    Remove old Event type

--- TCA/Sources/Analytics/Event.swift
+++ TCA/Sources/Analytics/Event.swift
@@ -1,25 +1,5 @@
 import FirebaseAnalytics
 
-//public struct Event {
-//	let name: String
-//	let parameters: [String: Any]?
-//
-//	public init(
-//		name: String,
-//		parameters: [String: Any]?
-//	) {
-//		self.name = name
-//		self.parameters = parameters
-//	}
-//
-//	public static func screenView(screenName: String, screenClass: String = "") -> Self {
-//		.init(name: AnalyticsEventScreenView, parameters: [
-//			AnalyticsParameterScreenName: screenName,
-//			AnalyticsParameterScreenClass: screenClass
-//		])
-//	}
-//}
-
 public enum Event {
 	case event(name: String, parameters: [String: Any]?)
 	case screen(screenName: String, className: String)
