1cba9bdcf25d3ace299a5c5d16dec7e5bc2bfe89
Oliver Foggin <oliverfoggin@me.com>
2022-03-06 23:46:33 +0000

    Package file format

--- TCA/Package.swift
+++ TCA/Package.swift
@@ -10,46 +10,28 @@ let package = Package(
 		.library(name: "NumberCore", targets: ["NumberCore"]),
 	],
 	dependencies: [
-		.package(
-			url: "https://github.com/pointfreeco/swift-composable-architecture",
-			from: "0.33.1"
-		),
-		.package(
-			url: "https://github.com/firebase/firebase-ios-sdk.git",
-			.branch("master")
-		),
+		.package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.33.1"),
+		.package(url: "https://github.com/firebase/firebase-ios-sdk.git", .branch("master")),
 	],
 	targets: [
 		.target(
 			name: "Analytics",
 			dependencies: [
-				.product(
-					name: "ComposableArchitecture",
-					package: "swift-composable-architecture"
-				),
-				.product(
-					name: "FirebaseAnalytics",
-					package: "firebase-ios-sdk"
-				),
+				.product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+				.product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
 			]
 		),
 		.target(
 			name: "NumberCore",
 			dependencies: [
 				"Analytics",
-				.product(
-					name: "ComposableArchitecture",
-					package: "swift-composable-architecture"
-				),
+				.product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
 			]
 		),
 		.testTarget(
 			name: "NumberCoreTests",
 			dependencies: [
-				.product(
-					name: "ComposableArchitecture",
-					package: "swift-composable-architecture"
-				),
+				.product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
 			]
 		)
 	]
