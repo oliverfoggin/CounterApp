9ab35894bc8442bdaa58b98cdf1866f6cb3c0e26
Oliver Foggin <oliverfoggin@me.com>
2022-03-06 16:40:15 +0000

    Initial commit

---
+++ NumberAppVanilla/NumberAppVanilla/NumberAppVanillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberAppVanillaApp.swift
+//  NumberAppVanilla
+//
+//  Created by Oliver Foggin on 06/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct NumberAppVanillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
